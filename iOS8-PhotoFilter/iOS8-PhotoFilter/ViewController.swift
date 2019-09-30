4d08afea4e1dab1684e6e56b6613aa34070df96c
Paul Solt <PaulSolt@gmail.com>
2019-09-30 12:21:38 -0400

    Starting project (iOS 13 only apparently)

---
+++ iOS8-PhotoFilter/iOS8-PhotoFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS8-PhotoFilter
+//
+//  Created by Paul Solt on 9/30/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
